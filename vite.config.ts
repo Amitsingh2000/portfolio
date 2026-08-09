36c2997b5d987b836491705068dc2c28d2e642a4
Amit Pardeshi <amit.pardeshi2000@gmail.com>
2026-08-10 00:26:58 +0530

    update depolyment config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
-  // Using relative base path './' so static assets load correctly on GitHub Pages regardless of domain/repo path
-  base: './',
-
   plugins: [react()],
-
-  optimizeDeps: {
-    exclude: ['lucide-react'],
-  },
-});
+  base: '/portfolio/',
+})
